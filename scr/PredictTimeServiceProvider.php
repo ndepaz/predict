a9e5e5c0451d806e0b0da0e5e0fb1d8cbaea7ff0
Noe <ndepaz2016@gmail.com>
2019-02-27 11:22:09 -0600

    added helpers

--- scr/PredictTimeServiceProvider.php
+++ scr/PredictTimeServiceProvider.php
@@ -11,8 +11,8 @@ use Illuminate\Support\ServiceProvider;
 class PredictTimeServiceProvider extends ServiceProvider
 {
     public function boot(){
-        $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
-
+//        $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
+        require __DIR__.'/helpers.php';
     }
     public function register(){
 
